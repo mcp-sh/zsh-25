41fab2b6fa2eff7da6ab3d59b3809f857b8e0cfc
Max Paeslack <mcp@macapa.me>
2024-09-09 20:15:18 +0800

    Restructured attendee list

--- db/seed.ts
+++ db/seed.ts
@@ -1,4 +1,4 @@
-import { db, Rsvps } from "astro:db";
+import { db, Rsvps, Lunch } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function () {
@@ -10,6 +10,14 @@ export default async function () {
       pax: 1,
       arrDate: "2024-10-18",
       depDate: "2024-10-21",
+    },
+  ]);
+  await db.insert(Lunch).values([
+    {
+      name: "Max Paeslack",
+      company: "ZERMA Shanghai",
+      email: "mcp@macapa.me",
+      pax: 1,
     },
   ]);
 }
--- src/pages/api/getAttendees.json.js
+++ src/pages/api/getAttendees.json.js
@@ -3,9 +3,12 @@ export async function GET() {
   const attendees = await db.select().from(Rsvps);
   const lunchAttendees = await db.select().from(Lunch);
 
-  return new Response(JSON.stringify([attendees, lunchAttendees]), {
-    headers: {
-      "Content-Type": "application/json",
+  return new Response(
+    JSON.stringify({ RSVP: attendees, LUNCH: lunchAttendees }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
     },
-  });
+  );
 }
